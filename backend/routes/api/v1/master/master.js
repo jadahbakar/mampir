4e5a39a585fdb84507937c89fa018dea4e19b346
dedystyawan <dedy@staff.dinus.ac.id>
2020-01-07 06:32:34 +0700

    wisata v1

--- backend/routes/api/v1/master/master.js
+++ backend/routes/api/v1/master/master.js
@@ -47,9 +47,93 @@ const getKelurahan = async (request, response, next) => {
   }
 }
 
+const getKategoriWisata = async (request, response, next) => {
+  const kategoriWisata = await db.any(
+    'SELECT * FROM mst.kategori_wisata_get()'
+  )
+  try {
+    response.json(kategoriWisata)
+  } catch (error) {
+    return response.status(400).send(error)
+  }
+}
+
+const getWisata = async (request, response, next) => {
+  const jenis = request.params.jenis
+  const search = request.params.search
+
+  let query
+  let values
+  if (jenis === 'propinsi') {
+    query = `SELECT wisata_id, wisata_nama, wisata_propinsi, wisata_kota, wisata_deskripsi, wisata_kategori, wisata_logitute, wisata_latitude, wisata_images 
+    FROM mst.wisata WHERE wisata_propinsi = $(search)`
+    values = [search]
+  }
+
+  if (jenis === 'kota') {
+    query = `SELECT wisata_id, wisata_nama, wisata_propinsi, wisata_kota, wisata_deskripsi, wisata_kategori, wisata_logitute, wisata_latitude, wisata_images 
+    FROM mst.wisata WHERE wisata_kota = $(search)`
+    values = [search]
+  }
+
+  if (jenis === 'kategori') {
+    query = `SELECT wisata_id, wisata_nama, wisata_propinsi, wisata_kota, wisata_deskripsi, wisata_kategori, wisata_logitute, wisata_latitude, wisata_images 
+    FROM mst.wisata WHERE wisata_kategori = $(search)`
+    values = [search]
+  }
+
+  const wisata = await db.any(query, values)
+  try {
+    response.json(wisata)
+  } catch (error) {
+    return response.status(400).send(error)
+  }
+}
+
+const postWisata = async (request, response, next) => {
+  const { nama, propinsi, kota, deskripsi, kategori, logitude, latitude, images } = request.body
+  const imagesJ = JSON.stringify(images)
+  const posting = await db.one(`INSERT INTO mst.wisata (wisata_nama, wisata_propinsi, wisata_kota, wisata_deskripsi, wisata_kategori, wisata_logitute, wisata_latitude, wisata_images )
+        VALUES ($(nama),$(propinsi),$(kota),$(deskripsi),$(kategori),$(logitude),$(latitude),$(images)) RETURNING wisata_id 
+        `, { nama, propinsi, kota, deskripsi, kategori, logitude, latitude, imagesJ })
+  try {
+    response.json(posting)
+  } catch (error) {
+    return response.status(400).send(error)
+  }
+}
+
+const putWisata = async (request, response, next) => {
+  const { nama, propinsi, kota, deskripsi, kategori, logitude, latitude, images } = request.body
+  const imagesJ = JSON.stringify(images)
+  const posting = await db.one(`INSERT INTO mst.wisata (wisata_nama, wisata_propinsi, wisata_kota, wisata_deskripsi, wisata_kategori, wisata_logitute, wisata_latitude, wisata_images )
+        VALUES ($(nama),$(propinsi),$(kota),$(deskripsi),$(kategori),$(logitude),$(latitude),$(images)) RETURNING wisata_id 
+        `, { nama, propinsi, kota, deskripsi, kategori, logitude, latitude, imagesJ })
+  try {
+    response.json(posting)
+  } catch (error) {
+    return response.status(400).send(error)
+  }
+}
+
+const deleteWisata = async (request, response, next) => {
+  const id = request.params.id
+  try {
+    await db.none('DELETE FROM mst.wisata WHERE wisata_id = $(id)', { id })
+    response.json('deleted')
+  } catch (error) {
+    return response.status(400).send(error)
+  }
+}
+
 router.get('/propinsi', getPropinsi)
 router.get('/kota/:propinsiId', getKota)
 router.get('/kecamatan/:kotaId', getKecamatan)
 router.get('/kelurahan/:kecamatanId', getKelurahan)
+router.get('/kategoriwisata', getKategoriWisata)
+router.get('/wisata/:jenis/:search', getWisata)
+router.post('/wisata', postWisata)
+router.put('/wisata/:id', putWisata)
+router.delete('/wisata/:id', deleteWisata)
 
 module.exports = router
